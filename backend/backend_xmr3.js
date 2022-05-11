615cb2b90170312473686ab2e7ffe8f83e703d87
spirobel <spirobel@protonmail.com>
2022-05-11 16:33:19 +0800

    await db promise

--- backend/backend_xmr3.js
+++ backend/backend_xmr3.js
@@ -18,10 +18,11 @@ module.exports.createPaymentUri = async function(wallet, standardAddress, amount
  //client.db('monerochan').collection('transactions').insertOne()
  module.exports.register = async function(wallet, db, amount, txHash, address, message, signature){
     wallet = await wallet
+    db = await db
     const check = await wallet.checkTxProof(txHash, address, message, signature) 
 
     if( check.isGood() && check.getReceivedAmount().compare(new BigInteger(amount)) >= 0){
-        await db('monerochan').collection('registrations').insertOne({
+        await db.collection('registrations').insertOne({
             txHash, address
         })
         return true
@@ -32,7 +33,8 @@ module.exports.createPaymentUri = async function(wallet, standardAddress, amount
 
 module.exports.login = async function(wallet, db, address, txHash, message, signature){
     wallet = await wallet
-    const regs = await db('monerochan').collection('registrations').find({txHash, address}).toArray()
+    db = await db
+    const regs = await db.collection('registrations').find({txHash, address}).toArray()
     if(!regs[0].txHash){ return false}
     const check = await wallet.checkSpendProof(regs[0].txHash, message, signature) 
     return check
